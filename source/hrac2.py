7797603f5ed1dd08a582ac3d11a60bfda262f5d6
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-02-08 08:55:45 +0100

    Konečně funkční kolize se zemí

--- source/hrac2.py
+++ source/hrac2.py
@@ -1,74 +1,90 @@
 import pygame
 import math
 
+
 class Hrac2():
     def __init__(self, x, y, sirka, vyska, speed):
         self.speed = speed
         self.rect = pygame.Rect(x, y, sirka, vyska)
-        self.image = pygame.image.load("tank_B_textury/Tank_B_les.png") 
-        self.image = pygame.transform.scale(self.image, (sirka, vyska))  
-        
-        self.velka_y = 0  # Vertikální rychlost (gravitace)
-        self.na_zemi = False 
-        
-    def pohni_se(self, klavesa, maska):
-        # Pohyb hráče
-        if klavesa[pygame.K_DOWN]:
-            self.rect.y += self.speed  
-        
-        if klavesa[pygame.K_LEFT]:
-            self.rect.x -= self.speed  
-        
-        if klavesa[pygame.K_RIGHT]:
-            self.rect.x += self.speed  
-
-        # Simulace gravitace
-        self.velka_y += 1
-        self.rect.y += self.velka_y
-        
-        
+        self.image = pygame.image.load("tank_B_textury/Tank_B_les.png")
+        self.image = pygame.transform.scale(self.image, (sirka, vyska))
+        self.sklon = 0.5
+        self.velka_y = 0
         self.na_zemi = False
+        self.smer_pohybu = 0  
 
-        # Test zda hráč není ve vzduchu 
-        if maska.overlap(pygame.mask.from_surface(self.image), (self.rect.x, self.rect.y + self.rect.height)):
-            self.na_zemi = True
-            self.rect.y = (self.rect.y // 5) * 5
-            self.velka_y = 0
-        
-        # Pokud je hráč na šikmé ploše
-        if self.na_zemi:
+    def pohni_se(self, klavesa, maska):
+        #ukládání pozice
+        original_x = self.rect.x
+        original_y = self.rect.y
+
+        # nastavení směru pohybu
+        self.smer_pohybu = 0
+        if klavesa[pygame.K_LEFT]:
+            self.smer_pohybu = -1
+            self.rect.x -= self.speed
+        if klavesa[pygame.K_RIGHT]:
+            self.smer_pohybu = 1
+            self.rect.x += self.speed
+
+       #kolize se zemí
+        if maska.overlap(pygame.mask.from_surface(self.image), (self.rect.x, self.rect.y)):
+            test_y = self.rect.y
+            # hledání cesty aby mohl na horu
+            for i in range(int(self.speed * 2)): 
+                test_y -= 1
+                if not maska.overlap(pygame.mask.from_surface(self.image), (self.rect.x, test_y)):
+                    self.rect.y = test_y
+                    break
+            else:
+                self.rect.x = original_x   #když nenajde vrátí se zpět
+       
+        # gravitace když není na zemi
+        if not self.na_zemi:
+            self.velka_y += 0.5
+        self.rect.y += self.velka_y
+        #kontrola kolize při pádu
+        if maska.overlap(pygame.mask.from_surface(self.image), (self.rect.x, self.rect.y)):
+            if self.velka_y > 0: 
+                while maska.overlap(pygame.mask.from_surface(self.image), (self.rect.x, self.rect.y)):
+                    self.rect.y -= 1
+                self.na_zemi = True
+                self.velka_y = 0
+            else: 
+                self.rect.y = original_y
+                self.velka_y = 0
+ 
+        if self.na_zemi and self.smer_pohybu != 0:
             self.pohyb_na_sikme_plosine(maska)
-        
-        # Omezení aby hráč neprošel skrz zem
-        if self.rect.top < 0:
-            self.rect.top = 0
-            self.na_zemi = True
-        
-        if self.rect.bottom > 720:
-            self.rect.bottom = 720
-            self.na_zemi = True
-        
-        if self.rect.left < 0:
-            self.rect.left = 0
-        
-        if self.rect.right > 1280:
-            self.rect.right = 1280
-    
+
+        test_rect = self.rect.copy()
+        test_rect.y += 1
+        if not maska.overlap(pygame.mask.from_surface(self.image), (test_rect.x, test_rect.y)):
+            self.na_zemi = False
+
+        self.rect.clamp_ip(pygame.Rect(0, 0, 1280, 720))
+
     def pohyb_na_sikme_plosine(self, maska):
-        # Získání pozice hráče
-        x, y = self.rect.topleft
+        #testovací bod před hráčem
+        test_ahead = self.rect.copy()
+        test_ahead.x += self.smer_pohybu * self.speed
+        found_surface = False
+        #hledá povrch nad nebo pod hráčem
+        for y_offset in range(-int(self.speed * 2), int(self.speed * 2)):
+            test_ahead.y = self.rect.y + y_offset
+            if maska.overlap(pygame.mask.from_surface(self.image), (test_ahead.x, test_ahead.y)):
+                target_y = test_ahead.y - 1 
+                #plynulí přechod na novou výšku
+                dy = target_y - self.rect.y
+                if abs(dy) > self.speed:
+                    dy = self.speed if dy > 0 else -self.speed
+                
+                self.rect.y += dy
+                found_surface = True
+                break
+        
+        if not found_surface:
+            self.na_zemi = False
 
-        if maska.overlap(pygame.mask.from_surface(self.image), (x, y)):
-            sklon = 0.5  # Sklon šikmé plochy
-            
-            if sklon > 0:  
-                self.rect.x += self.speed * math.cos(sklon)
-                self.rect.y -= self.speed * math.sin(sklon)
-            else:  
-                self.rect.x += self.speed * math.cos(sklon)
-                self.rect.y += self.speed * math.sin(sklon)
-
-            self.rect.y = round(self.rect.y)
-
-    def vykresli_se(self, screen):
+    def vykresli_se(self, screen, maska):
         screen.blit(self.image, self.rect)
--- source/main.py
+++ source/main.py
@@ -1,11 +1,10 @@
 import pygame
-from hrac import Hrac  
-from hrac2 import Hrac2  
+import math
+from hrac import Hrac
+from hrac2 import Hrac2
 
-# Inicializace pygame
 pygame.init()
 
-# Okno
 velikost_okna_x = 1280
 velikost_okna_y = 720
 screen = pygame.display.set_mode((velikost_okna_x, velikost_okna_y))
@@ -15,16 +14,10 @@ zem = pygame.image.load("./zem_textury/pozadi_chozeni_les.png").convert_alpha()
 zem_rect = zem.get_rect()
 zem_mask = pygame.mask.from_surface(zem)
 
-# Vytvoření povrchu pro masku
-mask_surface = pygame.Surface(zem.get_size())
-mask_surface.fill((0, 0, 0))
-mask_surface.blit(zem, (0, 0), special_flags=pygame.BLEND_RGBA_MAX)
+# Vytvoření hráčů
+hrac = Hrac(velikost_okna_x // 2, 0, 120, 80, 5)
+hrac2 = Hrac2(velikost_okna_x // 2, 0, 120, 80, 5)
 
-# Načtení textur pro hráče
-hrac = Hrac(velikost_okna_x // 2, 0, 120, 80, 5)  
-hrac2 = Hrac2(velikost_okna_x / 2, 0, 120, 80, 5)
-
-# FPS limit
 clock = pygame.time.Clock()
 status = True
 
@@ -35,23 +28,17 @@ while status:
         if event.type == pygame.QUIT:
             status = False
 
-    # Pohyb hráče
+    # Aktualizace hráčů
     klavesa = pygame.key.get_pressed()
     hrac.pohni_se(klavesa, zem_mask)
     hrac2.pohni_se(klavesa, zem_mask)
 
-    # Pohyb po šikmé plošině
-    hrac.pohyb_na_sikme_plosine(zem_mask)
-    hrac2.pohyb_na_sikme_plosine(zem_mask)
-
     # Vykreslování
-    screen.fill((0, 0, 0))
-    screen.blit(mask_surface, (0, 0))  
-    hrac.vykresli_se(screen)  
-    hrac2.vykresli_se(screen)
+    screen.fill((0, 100, 240))
+    screen.blit(zem, (0, 0))
+    hrac.vykresli_se(screen, zem_mask)
+    hrac2.vykresli_se(screen, zem_mask)
 
     pygame.display.update()
 
 pygame.quit()
-
-  
