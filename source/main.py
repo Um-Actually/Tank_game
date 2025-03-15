fae6381c62b84a60104ed038e8e7c30bad3cd648
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-03-15 08:49:19 +0100

    funkční tlačítka na změnu vzhledu tanku

--- source/main.py
+++ source/main.py
@@ -4,11 +4,11 @@ import math
 from hrac import Hrac
 from power_upy import PowerUpManager
 from delo_hrac1 import delo
-
+hlavni_smycka=True
 velikost_okna_x = 1920
 velikost_okna_y = 1080
 screen = pygame.display.set_mode((velikost_okna_x, velikost_okna_y))
-pygame.display.set_caption("Ahoj světe!")
+pygame.display.set_caption("war of tanks")
 
 # Klávesy pro ovládání hráčů
 left_h1 = "a"
@@ -49,30 +49,14 @@ d_2_les = pygame.image.load("tank_B_textury/delo_tank_B_les.png")
 d_2_poust = pygame.image.load("tank_B_textury/delo_tank_B_poust.png")
 d_2_zima = pygame.image.load("tank_B_textury/delo_tank_B_zima.png")
 
-# Náhodný výběr textur pro hráče 1
-textura_hrac1 = random.choice([t_1_les, t_1_poust, t_1_zima])
-if textura_hrac1 == t_1_les:
-    textura_delo1 = d_1_les
-elif textura_hrac1 == t_1_poust:
-    textura_delo1 = d_1_poust
-else:
-    textura_delo1 = d_1_zima
-
-# Náhodný výběr textur pro hráče 2
-textura_hrac2 = random.choice([t_2_les, t_2_poust, t_2_zima])
-if textura_hrac2 == t_2_les:
-    textura_delo2 = d_2_les
-elif textura_hrac2 == t_2_poust:
-    textura_delo2 = d_2_poust
-else:
-    textura_delo2 = d_2_zima
-
-
 zem_les=pygame.image.load("./zem_textury/zem_chozeni_les.png").convert_alpha()
 zem_poust=pygame.image.load("./zem_textury/zem_chozeni_poust.png").convert_alpha()
 zem_zima=pygame.image.load("./zem_textury/zem_chozeni_zima.png").convert_alpha()
 
-
+textura_hrac1=t_1_les
+textura_hrac2=t_2_les
+textura_delo1=d_1_les
+textura_delo2=d_2_les
 # Načítání pozadí a masky
 zem = random.choice([zem_les,zem_poust,zem_zima])
 zem_rect = zem.get_rect()
@@ -114,12 +98,29 @@ tlacitko_1=pygame.Rect((velikost_okna_x/2 - 100, velikost_okna_y/2 - 50, 200, 10
 tlacitko_2=pygame.Rect((velikost_okna_x/2 - 100, velikost_okna_y/2 + 75, 200, 100))
 tlacitko_3=pygame.Rect((velikost_okna_x/2 - 100, velikost_okna_y/2 + 200, 200, 100))
 
+hrac1_tlacitko_1=pygame.Rect((0 + 100, velikost_okna_y/2 - 50, 200, 100))
+hrac1_tlacitko_2=pygame.Rect((0 + 100, velikost_okna_y/2 + 75, 200, 100))
+hrac1_tlacitko_3=pygame.Rect((0 + 100, velikost_okna_y/2 + 200, 200, 100))
+
+hrac2_tlacitko_1=pygame.Rect((velikost_okna_x - 300, velikost_okna_y/2 - 50, 200, 100))
+hrac2_tlacitko_2=pygame.Rect((velikost_okna_x - 300, velikost_okna_y/2 + 75, 200, 100))
+hrac2_tlacitko_3=pygame.Rect((velikost_okna_x - 300, velikost_okna_y/2 + 200, 200, 100))
+
 hlavni_smycka=True
 
 while hlavni_smycka:
     status = False
     menu_skiny=False
     menu=True
+
+    hrac = Hrac(velikost_okna_x // 4, 0, 120, 80, 2, textura_hrac1, 
+            left_h1, right_h1, up_h1, down_h1, textura_delo1, 
+            fire_h1, std_h1, velky_h1, rychly_h1, smoke_h1)
+
+    hrac2 = Hrac(velikost_okna_x * 3 // 4, 0, 120, 80, 2, textura_hrac2, 
+            left_h2, right_h2, up_h2, down_h2, textura_delo2, 
+            fire_h2, std_h2, velky_h2, rychly_h2 ,smoke_h2)
+
     while menu:
         pozice_mys= pygame.mouse.get_pos()
         mys_nad_tlacitkem_1=tlacitko_1.collidepoint(pozice_mys)
@@ -151,10 +152,13 @@ while hlavni_smycka:
 
     while menu_skiny:
         pozice_mys= pygame.mouse.get_pos()
-        mys_nad_tlacitkem_1=tlacitko_1.collidepoint(pozice_mys)
-        mys_nad_tlacitkem_2=tlacitko_2.collidepoint(pozice_mys)
-        mys_nad_tlacitkem_3=tlacitko_3.collidepoint(pozice_mys)
+        hrac1_mys_nad_tlacitkem_1=hrac1_tlacitko_1.collidepoint(pozice_mys)
+        hrac1_mys_nad_tlacitkem_2=hrac1_tlacitko_2.collidepoint(pozice_mys)
+        hrac1_mys_nad_tlacitkem_3=hrac1_tlacitko_3.collidepoint(pozice_mys)
 
+        hrac2_mys_nad_tlacitkem_1=hrac2_tlacitko_1.collidepoint(pozice_mys)
+        hrac2_mys_nad_tlacitkem_2=hrac2_tlacitko_2.collidepoint(pozice_mys)
+        hrac2_mys_nad_tlacitkem_3=hrac2_tlacitko_3.collidepoint(pozice_mys)
         for udalost in pygame.event.get():
             if udalost.type == pygame.QUIT:
                 menu = False
@@ -165,84 +169,54 @@ while hlavni_smycka:
                     status=False
 
             elif udalost.type==pygame.MOUSEBUTTONDOWN:
-                if mys_nad_tlacitkem_1:
-                    status=True
-                    menu=False
-                elif mys_nad_tlacitkem_2:
-                    menu_skiny=True
-
-                elif mys_nad_tlacitkem_3:
-                    status=False
-                    menu=False
-            
+                if hrac1_mys_nad_tlacitkem_1:
+                    textura_hrac1=t_1_les
+                    textura_delo1=d_1_les
+                elif hrac1_mys_nad_tlacitkem_2:
+                    textura_hrac1=t_1_poust
+                    textura_delo1=d_1_poust
+                elif hrac1_mys_nad_tlacitkem_3:
+                    textura_hrac1=t_1_zima
+                    textura_delo1=d_1_zima
+                elif hrac2_mys_nad_tlacitkem_1:
+                    textura_hrac2=t_2_les
+                    textura_delo2=d_2_les
+                elif hrac2_mys_nad_tlacitkem_2:
+                    textura_hrac2=t_2_poust
+                    textura_delo2=d_2_poust
+                elif hrac2_mys_nad_tlacitkem_3:
+                    textura_hrac2=t_2_zima
+                    textura_delo2=d_2_zima
 
         screen.fill((255, 255, 255))
-        pygame.draw.rect(screen, (0, 255, 0), (velikost_okna_x - 100, velikost_okna_y/2 - 50, 200, 100))
-        pygame.draw.rect(screen, (0, 255, 0), (velikost_okna_x - 100, velikost_okna_y/2 + 75, 200, 100))
-        pygame.draw.rect(screen, (0, 255, 0), (velikost_okna_x - 100, velikost_okna_y/2 + 200, 200, 100))
+        pygame.draw.rect(screen, (0, 255, 0), (velikost_okna_x - 300, velikost_okna_y/2 - 50, 200, 100))
+        pygame.draw.rect(screen, (0, 255, 0), (velikost_okna_x - 300, velikost_okna_y/2 + 75, 200, 100))
+        pygame.draw.rect(screen, (0, 255, 0), (velikost_okna_x - 300, velikost_okna_y/2 + 200, 200, 100))
+        screen.blit(pygame.transform.flip(t_2_les, True, False),(velikost_okna_x - 273, velikost_okna_y/2 - 45, 200, 100))
+        screen.blit(pygame.transform.flip(t_2_poust, True, False),(velikost_okna_x - 273, velikost_okna_y/2 + 80, 200, 100))
+        screen.blit(pygame.transform.flip(t_2_zima, True, False),(velikost_okna_x - 273, velikost_okna_y/2 + 205, 200, 100))
 
         pygame.draw.rect(screen, (0, 255, 0), (0 + 100, velikost_okna_y/2 - 50, 200, 100))
         pygame.draw.rect(screen, (0, 255, 0), (0 + 100, velikost_okna_y/2 + 75, 200, 100))
         pygame.draw.rect(screen, (0, 255, 0), (0 + 100, velikost_okna_y/2 + 200, 200, 100))
+        screen.blit(t_1_les, (0 + 145, velikost_okna_y / 2 - 35))
+        screen.blit(t_1_poust, (0 + 145, velikost_okna_y / 2 + 90))
+        screen.blit(t_1_zima, (0 + 145, velikost_okna_y / 2 + 215))
+
+
+
         pygame.display.flip()
 
 
+    hrac = Hrac(velikost_okna_x // 4, 0, 120, 80, 2, textura_hrac1, 
+            left_h1, right_h1, up_h1, down_h1, textura_delo1, 
+            fire_h1, std_h1, velky_h1, rychly_h1, smoke_h1)
+
+    hrac2 = Hrac(velikost_okna_x * 3 // 4, 0, 120, 80, 2, textura_hrac2, 
+            left_h2, right_h2, up_h2, down_h2, textura_delo2, 
+            fire_h2, std_h2, velky_h2, rychly_h2 ,smoke_h2)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    hra_bezi = True
     konec_hry = False
     winner = None
     clock = pygame.time.Clock()
